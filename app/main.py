50eaa7745d45c5a164f9afe6857c95b6936ae776
Zeshan Haider <mhaider.zeshan@gmail.com>
2025-11-03 12:36:19 +0500

    add test-db endpoint to main.py file

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ from app.routers.auth_google import router as oauth_router
 from app.core.config import settings
 from app.routers.users import router as user_router
 from app.routers.local_auth import router as local_auth_router
+import mysql.connector
+import os
+from typing import Optional, Tuple, Any
+
 
 SECRET_KEY = settings.SECRET_KEY.get_secret_value()
 
@@ -32,3 +36,31 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app = FastAPI()
+
+
+@app.get("/test-db")
+def test_db():
+    try:
+        conn = mysql.connector.connect(
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME"),
+            ssl_ca=os.getenv("DB_SSL_CA")
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT NOW();")
+
+        result: Optional[Tuple[Any, ...]] = cursor.fetchone()  # type: ignore
+        if result:
+            print(result[0])
+
+        else:
+            print("No result found")
+
+    except Exception as e:
+        return {"success": False, "error": str(e)}
